afa32ac86bb3a7d5edecc5eab3fa91a040cd63a3
siloneco <siloneco4646@gmail.com>
2020-03-09 21:58:13 +0900

    [Fix] 非同期の方法を変更

--- src/main/java/jp/azisaba/lgw/rankingdisplayer/DisplayListener.java
+++ src/main/java/jp/azisaba/lgw/rankingdisplayer/DisplayListener.java
@@ -1,19 +1,12 @@
 package jp.azisaba.lgw.rankingdisplayer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-
+import com.google.common.base.Strings;
+import jp.azisaba.lgw.kdstatus.KDStatusReloaded;
+import jp.azisaba.lgw.kdstatus.sql.KillRankingData;
+import jp.azisaba.lgw.kdstatus.utils.TimeUnit;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -23,16 +16,9 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.BlockIterator;
 
-import com.google.common.base.Strings;
-
-import lombok.RequiredArgsConstructor;
-
-import jp.azisaba.lgw.kdstatus.KDStatusReloaded;
-import jp.azisaba.lgw.kdstatus.sql.KillRankingData;
-import jp.azisaba.lgw.kdstatus.utils.TimeUnit;
+import java.util.*;
 
 @RequiredArgsConstructor
 public class DisplayListener implements Listener {
@@ -56,21 +42,21 @@ public class DisplayListener implements Listener {
 
     @EventHandler
     public void onInteractUpdateButton(PlayerInteractEvent e) {
-        if ( e.getAction() != Action.RIGHT_CLICK_BLOCK ) {
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
 
         Player p = e.getPlayer();
         Block b = e.getClickedBlock();
 
-        if ( !b.getType().toString().endsWith("BUTTON") ) {
+        if (!b.getType().toString().endsWith("BUTTON")) {
             return;
         }
 
-        if ( RankingDisplayer.getPluginConfig().updateButtonList.contains(b.getLocation()) ) {
+        if (RankingDisplayer.getPluginConfig().updateButtonList.contains(b.getLocation())) {
             e.setCancelled(true);
 
-            if ( updatedTime.containsKey(p) && updatedTime.get(p) + 5000 > System.currentTimeMillis() ) {
+            if (updatedTime.containsKey(p) && updatedTime.get(p) + 5000 > System.currentTimeMillis()) {
                 p.sendMessage(ChatColor.RED + "更新ボタンを連打しないでください。");
                 return;
             }
@@ -82,19 +68,19 @@ public class DisplayListener implements Listener {
 
     @EventHandler
     public void onChangeDisplayType(PlayerInteractEvent e) {
-        if ( !e.getAction().toString().startsWith("RIGHT_CLICK") ) {
+        if (!e.getAction().toString().startsWith("RIGHT_CLICK")) {
             return;
         }
         Player p = e.getPlayer();
 
-        if ( processingPlayers.contains(p) ) {
+        if (processingPlayers.contains(p)) {
             return;
         }
 
         Location clickedLoc = getLookingAirBlock(p).getLocation();
         Location displayLoc = RankingDisplayer.getPluginConfig().displayLocation.clone();
 
-        if ( clickedLoc.getWorld() != displayLoc.getWorld() ) {
+        if (clickedLoc.getWorld() != displayLoc.getWorld()) {
             return;
         }
 
@@ -105,7 +91,7 @@ public class DisplayListener implements Listener {
         clickedLoc.setZ(clickedLoc.getBlockZ());
         displayLoc.setY(0);
 
-        if ( clickedLoc.distance(displayLoc) <= 2 && isYArea ) {
+        if (clickedLoc.distance(displayLoc) <= 2 && isYArea) {
             displayTypeMap.put(p, getNext(displayTypeMap.getOrDefault(p, null)));
 
             p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, 1, 1);
@@ -120,15 +106,15 @@ public class DisplayListener implements Listener {
         Block b = iter.next();
 
         int count = 0;
-        while ( iter.hasNext() ) {
+        while (iter.hasNext()) {
             count++;
             b = iter.next();
 
-            if ( b.getType() != Material.AIR ) {
+            if (b.getType() != Material.AIR) {
                 return b;
             }
 
-            if ( count >= 3 ) {
+            if (count >= 3) {
                 break;
             }
         }
@@ -141,7 +127,7 @@ public class DisplayListener implements Listener {
         World to = p.getWorld();
 
         Location display = RankingDisplayer.getPluginConfig().displayLocation;
-        if ( display.getWorld() != to ) {
+        if (display.getWorld() != to) {
             return;
         }
 
@@ -154,7 +140,7 @@ public class DisplayListener implements Listener {
         World world = p.getWorld();
 
         Location display = RankingDisplayer.getPluginConfig().displayLocation;
-        if ( display.getWorld() != world ) {
+        if (display.getWorld() != world) {
             return;
         }
 
@@ -163,7 +149,7 @@ public class DisplayListener implements Listener {
 
     public void displayRankingForPlayerAsync(Player p, boolean ignoreCache) {
 
-        if ( holoMap.containsKey(p) ) {
+        if (holoMap.containsKey(p)) {
             holoMap.get(p).removeAll();
         }
 
@@ -177,7 +163,7 @@ public class DisplayListener implements Listener {
     }
 
     private void displayRanking(DisplayType type, Hologram holo, Player p, boolean ignoreCache) {
-        new Thread(() -> {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             long start = System.currentTimeMillis();
 
             holo.setLine(0, HEADER);
@@ -191,16 +177,16 @@ public class DisplayListener implements Listener {
             String formattedLastUpdated = null;
 
             // KDStatusReloadedがない場合は取得
-            if ( kdsPlugin == null ) {
+            if (kdsPlugin == null) {
                 // 取得し、失敗したら不明とする
-                if ( !getKDSPlugin() ) {
+                if (!getKDSPlugin()) {
                     formattedLastUpdated = "不明";
                 }
             }
 
-            if ( kdsPlugin != null ) {
+            if (kdsPlugin != null) {
                 long updatedBefore = System.currentTimeMillis() - kdsPlugin.getSaveTask().getLastSavedTime();
-                if ( updatedBefore + (1000 * 5) > System.currentTimeMillis() ) {
+                if (updatedBefore + (1000 * 5) > System.currentTimeMillis()) {
                     formattedLastUpdated = "今";
                 } else {
                     formattedLastUpdated = DateFormatUtils.format(new Date(updatedBefore)) + "前";
@@ -219,15 +205,16 @@ public class DisplayListener implements Listener {
             plugin.getLogger().info("Displayed " + type.toString() + " Ranking for " + p.getName() + " async ("
                     + (end - start - (endedSynchro - waitingSynchro) + rankingDataFetchTime) + "ms)");
 
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if ( processingPlayers.contains(p) ) {
-                        processingPlayers.remove(p);
-                    }
-                }
-            }.runTaskLater(plugin, 0);
-        }).start();
+            try {
+                Thread.sleep(1000 * 5);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                processingPlayers.remove(p);
+            }, 0);
+        });
     }
 
     private void setRankingData(Hologram holo, Player p, List<KillRankingData> dataList, TimeUnit unit) {
@@ -236,9 +223,9 @@ public class DisplayListener implements Listener {
         int before = Integer.MIN_VALUE;
         boolean containsHim = false;
 
-        for ( KillRankingData data : dataList ) {
+        for (KillRankingData data : dataList) {
 
-            if ( num >= 7 ) {
+            if (num >= 7) {
                 break;
             }
 
@@ -246,18 +233,18 @@ public class DisplayListener implements Listener {
             UUID uuid = data.getUuid();
             String playerName = data.getName();
 
-            if ( kill <= 0 ) {
+            if (kill <= 0) {
                 break;
             }
 
             num++;
 
             boolean hide = RankingHideManager.isHiding(uuid);
-            if ( hide && !playerName.equals(p.getName()) ) {
+            if (hide && !playerName.equals(p.getName())) {
                 playerName = ChatColor.DARK_RED + "{匿名プレイヤー}";
             }
 
-            if ( kill != before ) {
+            if (kill != before) {
                 rank = num;
                 before = kill;
             }
@@ -265,12 +252,12 @@ public class DisplayListener implements Listener {
             String line = ChatColor.YELLOW + "" + rank + "位 " + ChatColor.GOLD + "{PLAYER}" + ChatColor.RED + ": "
                     + ChatColor.AQUA + kill + " kill(s)";
 
-            if ( playerName.equals(p.getName()) ) {
+            if (playerName.equals(p.getName())) {
                 line = ChatColor.BLUE + "YOU" + ChatColor.RED + " » " + line;
                 line = line.replace("{PLAYER}", p.getName());
                 containsHim = true;
 
-                if ( hide ) {
+                if (hide) {
                     line = ChatColor.DARK_RED + "(Hide) " + line;
                 }
             } else {
@@ -280,17 +267,17 @@ public class DisplayListener implements Listener {
             holo.addLine(line);
         }
 
-        while ( num < 7 ) {
+        while (num < 7) {
             num++;
             holo.addLine(ChatColor.YELLOW + "" + num + "位 " + ChatColor.GOLD + "なし");
         }
 
         holo.addLine(ChatColor.AQUA + StringUtils.repeat("=", 25));
 
-        if ( !containsHim ) {
+        if (!containsHim) {
             int ranking = kdsPlugin.getKdDataContainer().getRanking(p.getUniqueId(), unit);
             String rankingStr = "" + ranking;
-            if ( ranking <= 0 || kdsPlugin.getKdDataContainer().getPlayerData(p, true).getKills(unit) <= 0 ) {
+            if (ranking <= 0 || kdsPlugin.getKdDataContainer().getPlayerData(p, true).getKills(unit) <= 0) {
                 rankingStr = "-";
             }
 
@@ -299,7 +286,7 @@ public class DisplayListener implements Listener {
             String line = ChatColor.BLUE + "YOU" + ChatColor.RED + " » " + ChatColor.YELLOW + "" + rankingStr + "位 " +
                     ChatColor.GOLD + p.getName() + ChatColor.RED + ": " + ChatColor.AQUA + kills + " kill(s)";
 
-            if ( RankingHideManager.isHiding(p.getUniqueId()) ) {
+            if (RankingHideManager.isHiding(p.getUniqueId())) {
                 line = ChatColor.DARK_RED + "(Hide) " + line;
             }
             holo.addLine(line);
@@ -310,14 +297,14 @@ public class DisplayListener implements Listener {
         long start = System.currentTimeMillis();
 
         // KDStatusReloadedがない場合は取得
-        if ( kdsPlugin == null || !kdsPlugin.isEnabled() ) {
+        if (kdsPlugin == null || !kdsPlugin.isEnabled()) {
             // 取得し、失敗したらエラー
-            if ( !getKDSPlugin() ) {
+            if (!getKDSPlugin()) {
                 throw new IllegalStateException("Failed to get plugin \"KDStatusReloaded\"");
             }
         }
 
-        if ( ignoreCache || lastUpdated.getOrDefault(type, -1L) + cacheHoldMilliSec < System.currentTimeMillis() ) {
+        if (ignoreCache || lastUpdated.getOrDefault(type, -1L) + cacheHoldMilliSec < System.currentTimeMillis()) {
             List<KillRankingData> dataList = kdsPlugin.getKdDataContainer().getTopKillRankingData(type.getKdStatusTimeUnit(), 7);
             dataMap.put(type, dataList);
 
@@ -327,21 +314,21 @@ public class DisplayListener implements Listener {
     }
 
     public void removeAllBoards() {
-        for ( Hologram holo : holoMap.values() ) {
+        for (Hologram holo : holoMap.values()) {
             holo.removeAll();
         }
     }
 
     private String getFooter(DisplayType type) {
         StringBuilder builder = new StringBuilder();
-        for ( DisplayType type2 : DisplayType.values() ) {
-            if ( type2 != type ) {
+        for (DisplayType type2 : DisplayType.values()) {
+            if (type2 != type) {
                 builder.append(ChatColor.GRAY);
             } else {
-                builder.append(ChatColor.GREEN + "" + ChatColor.BOLD);
+                builder.append(ChatColor.GREEN.toString()).append(ChatColor.BOLD.toString());
             }
 
-            builder.append(type2.toString().substring(0, 1) + type2.toString().substring(1).toLowerCase() + " ");
+            builder.append(type2.toString().substring(0, 1)).append(type2.toString().substring(1).toLowerCase()).append(" ");
         }
 
         return builder.toString().trim();
@@ -351,13 +338,13 @@ public class DisplayListener implements Listener {
         // Pluginを取得
         Plugin pl = Bukkit.getPluginManager().getPlugin("KDStatusReloaded");
         // nullならreturn false
-        if ( pl == null ) {
+        if (pl == null) {
             return false;
         }
         // 代入
         kdsPlugin = (KDStatusReloaded) pl;
         // 無効化されていたらreturn false
-        if ( !kdsPlugin.isEnabled() ) {
+        if (!kdsPlugin.isEnabled()) {
             return false;
         }
         return true;
@@ -365,12 +352,12 @@ public class DisplayListener implements Listener {
 
     private DisplayType getNext(DisplayType type) {
         List<DisplayType> values = Arrays.asList(DisplayType.values());
-        if ( type == null ) {
+        if (type == null) {
             return values.get(1);
         }
         int index = values.indexOf(type) + 1;
 
-        if ( values.size() <= index ) {
+        if (values.size() <= index) {
             index = 0;
         }
 
