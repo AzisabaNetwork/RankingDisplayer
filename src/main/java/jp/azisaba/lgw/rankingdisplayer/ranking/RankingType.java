4f28646bd8420af299c2e8cde9dbbe50eb50ebe3
sysnote8main <sysnote8main@gmail.com>
2024-10-06 16:47:27 +0900

    [Add] Convert method for RankingType from String

--- src/main/java/jp/azisaba/lgw/rankingdisplayer/ranking/RankingType.java
+++ src/main/java/jp/azisaba/lgw/rankingdisplayer/ranking/RankingType.java
@@ -2,21 +2,37 @@ package jp.azisaba.lgw.rankingdisplayer.ranking;
 
 import jp.azisaba.lgw.kdstatus.utils.TimeUnit;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.Nullable;
 
-@RequiredArgsConstructor
+import java.util.HashMap;
+
 public enum RankingType {
-
     DAILY(TimeUnit.DAILY, "Daily"),
     MONTHLY(TimeUnit.MONTHLY, "Monthly"),
     TOTAL(TimeUnit.LIFETIME, "Total");
+    private static final HashMap<String, RankingType> convertMap = new HashMap<>();
 
     @Getter
     private final TimeUnit kdStatusTimeUnit;
     private final String name;
 
+    RankingType(TimeUnit kdStatusTimeUnit, String name) {
+        this.kdStatusTimeUnit = kdStatusTimeUnit;
+        this.name = name;
+        addConversion(name, this);
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    private static void addConversion(String name, RankingType type) {
+        convertMap.put(name, type);
+    }
+
+    @Nullable
+    public static RankingType getType(String name) {
+        return convertMap.get(name);
     }
 }
