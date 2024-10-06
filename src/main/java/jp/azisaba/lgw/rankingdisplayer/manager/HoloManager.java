f7d36301c9578434752bef6d72ee6597996bfd50
sysnote8main <sysnote8main@gmail.com>
2024-10-06 20:30:12 +0900

    [Add] Manager of holograms in the world

---
+++ src/main/java/jp/azisaba/lgw/rankingdisplayer/manager/HoloManager.java
@@ -0,0 +1,58 @@
+package jp.azisaba.lgw.rankingdisplayer.manager;
+
+import eu.decentsoftware.holograms.api.DHAPI;
+import eu.decentsoftware.holograms.api.holograms.Hologram;
+import jp.azisaba.lgw.rankingdisplayer.holo.RankingHolo;
+import org.bukkit.Location;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HoloManager {
+    /**
+     * Prefix of hologram's name
+     */
+    private static final String HOLO_PREFIX = "killranking";
+
+    /**
+     * Key: Location of hologram
+     * Value: Milliseconds of last updated time
+     */
+    private static final ConcurrentHashMap<String, Long> lastUpdateMap = new ConcurrentHashMap<>();
+
+    public static boolean addHolo(Location holoLocation) {
+        String _holoName = getHoloName(holoLocation);
+        if(lastUpdateMap.containsKey(_holoName)) {
+            return false;
+        }
+        lastUpdateMap.put(_holoName, System.currentTimeMillis());
+        Hologram holo = DHAPI.createHologram(_holoName, holoLocation, true);
+        return RankingHolo.setRanking(holo);
+    }
+
+    public static void removeHolo(String holoName) {
+        DHAPI.removeHologram(holoName);
+        lastUpdateMap.remove(holoName);
+    }
+
+    public static void removeAllHolo() {
+        for(String key: lastUpdateMap.keySet()) {
+            removeHolo(key);
+        }
+    }
+
+    public static Set<String> getAllHoloNames() {
+        return new HashSet<>(lastUpdateMap.keySet());
+    }
+
+    private static String getHoloName(Location location) {
+        return String.format(
+                "%s_%d_%d_%d",
+                HOLO_PREFIX,
+                location.getBlockX(),
+                location.getBlockY(),
+                location.getBlockZ()
+        );
+    }
+}
