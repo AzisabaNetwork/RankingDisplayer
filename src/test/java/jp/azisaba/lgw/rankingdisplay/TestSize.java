62498dff636a61cab13d8035755551b7bd44945f
sysnote8main <sysnote8main@gmail.com>
2024-10-27 16:07:11 +0900

    [Add] JUnit Test Platform with Template

---
+++ src/test/java/jp/azisaba/lgw/rankingdisplay/TestSize.java
@@ -0,0 +1,7 @@
+package jp.azisaba.lgw.rankingdisplay;
+
+public class TestSize {
+    public static final String SMALL = "small";
+    public static final String MEDIUM = "medium";
+    public static final String LARGE = "large";
+}
